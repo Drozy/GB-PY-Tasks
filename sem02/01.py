e14734a9916af2fb3d31627c2d708c6e3d0d2c3e
Yakovlev Andrew <drozy@yandex.ru>
2023-01-29 21:53:50 +0300

    Добавлены черновики 2 семинара

---
+++ sem02/01.py
@@ -0,0 +1,9 @@
+# По данному целому неотрицательному n вычислите значение n!.
+# N! = 1 * 2 * 3 * … * N (произведение всех чисел от 1 до N) 0! = 1
+
+number = int(input(f'Введите положительное число: '))
+factorial = 1
+for i in range(1, number+1):
+    factorial *= i
+
+print(f'{number}! = {factorial}')
---
+++ sem02/02.py
@@ -0,0 +1,25 @@
+# Дано натуральное число A > 1. Определите, каким по счету числом Фибоначчи оно является, то есть
+# выведите такое число n, что φ(n)=A. Если А не является числом Фибоначчи, выведите число -1.
+# Input: 5
+# Output: 6
+
+def chkFibonacci(number):
+    a = 0
+    b = 1
+    c = a + b
+    count = 3
+    if number < 2:
+        return number
+    while c <= number:
+        if c == number:
+            return count
+        else:
+            a = b
+            b = c
+            c = a + b
+            count += 1
+    return -1
+
+
+num = int(input(f'Введите число A > 1: '))
+print(chkFibonacci(num))
