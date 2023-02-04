c9a681271142e55364fd411a563aa1afd5511821
Yakovlev Andrew <drozy@yandex.ru>
2023-02-04 17:15:41 +0300

    Исправлен task1_2

--- sem01/task1_2.py
+++ sem01/task1_2.py
@@ -1,12 +1,15 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-x = bool(input('Введите X: '))
-y = bool(input('Введите Y: '))
-z = bool(input('Введите Z: '))
-
-left = not (x or y or z)
-right = not x and not y and not z
-if  left == right:
-    print('Утверждение истинно')
+flag = True
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            left = not (x or y or z)
+            right = not x and not y and not z
+            f = left == right
+            if  not f:
+                flag = False
+if flag:
+    print('Истина')
 else:
-    print('Утверждение ложно')
+    print('Ложь')
